9e51e8248bce34a552da31052fffb14bb8934b4f
Dilshodbek Akbarov <dilshod.akbarov.2003@gmail.com>
2025-05-30 03:26:25 +0500

    update

--- app/routes/favourite.jsx
+++ app/routes/favourite.jsx
@@ -1,7 +1,16 @@
-import Fav from "../components/Fav/Fav";
+import { useSelector } from "react-redux";
+import genie from "../assets/genie.webp"
+import Collection from "../components/Home/Collection"
 
 export default function Favourite() {
+  const wishes = useSelector((state) => state.cart.wishes);
   return (
-    <Fav />
-  )
+    <div className="home-page" style={{ position: "relative" }}>
+      <h2>Saralanganlar</h2>
+      {
+        wishes?.length !== 0 || <img src={genie} alt="" className="noItem-image"/>
+      }
+      <Collection all={wishes}/>
+    </div>
+  );
 }
